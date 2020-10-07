fa97283486da39a34c4ec94838fa7559b3c9ea14
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-10-07 12:46:42 -0300

    Add pinned project

--- src/components/content.js
+++ src/components/content.js
@@ -7,6 +7,14 @@ import ContentWithAside from "./content-with-aside"
 function Content() {
   return (
     <>
+      <Item
+        title="Code Hike"
+        date="2020/06/15"
+        url="https://codehike.org"
+        type="Project"
+        description="Marvellous code walkthroughs"
+        pinned
+      />
       {data.map((item, i) => (
         <Item key={i} {...item} />
       ))}
@@ -14,7 +22,7 @@ function Content() {
   )
 }
 
-function Item({ title, date, url, type, description }) {
+function Item({ title, date, url, type, description, pinned }) {
   const formattedDate = new Date(date).toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
@@ -56,13 +64,25 @@ function Item({ title, date, url, type, description }) {
                 display: ["inline", "block"],
               }}
             >
-              {type}
+              {pinned && <Pin />} {type}
             </Styled.h4>
             <small sx={{ ml: [2, 0] }}>{formattedDate}</small>
           </div>
         }
       />
     </div>
+  )
+}
+
+function Pin() {
+  return (
+    <svg viewBox="0 0 24 24" style={{ height: "1em", verticalAlign: "middle" }}>
+      <title>Pinned Project</title>
+      <path
+        d="M20.235 14.61c-.375-1.745-2.342-3.506-4.01-4.125l-.544-4.948 1.495-2.242a.763.763 0 00.037-.787.762.762 0 00-.675-.403h-9.14a.765.765 0 00-.638 1.191l1.498 2.247-.484 4.943c-1.668.62-3.633 2.38-4.004 4.116a.726.726 0 00.132.594c.103.132.304.29.68.29H8.64l2.904 6.712a.498.498 0 00.918 0l2.903-6.713h4.057c.376 0 .576-.156.68-.286a.74.74 0 00.135-.59z"
+        fill="currentColor"
+      ></path>
+    </svg>
   )
 }
 
